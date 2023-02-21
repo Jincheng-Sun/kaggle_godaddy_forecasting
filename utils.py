1aeddd5c7f49b881ca104d7834cb4b561345a99e
Jincheng Sun <sjc951213@gmail.com>
2023-02-20 23:44:03 -0500

    update utils; add lightgbm_baseline notebook

--- utils.py
+++ utils.py
@@ -16,12 +16,22 @@ def random_masking(df, columns: list, random_seed: int = 42, n_rep: int = 1, thr
     return pd.concat(replicas)
 
 
-def transpose_columns(df, idx: str = None, columns: list = None, col_f: callable = None) -> pd.Series:
+def transpose_columns(df, idx: str = None, columns: list = None, col_name: str = None, col_f: callable = None) -> pd.Series:
     df_copy = df.copy()
     if idx is not None:
-        df_copy = df_copy.set_index()
+        df_copy = df_copy.set_index(idx)
     if columns is not None:
         df_copy = df_copy[columns]
+    if col_name is not None:
+        df_copy.columns.name = col_name
     if col_f is not None:
         df_copy.columns = df_copy.columns.map(col_f)
     return df_copy.stack()
+
+
+def lagging_on(df, groupby: list, columns: list, n_lag: int) -> pd.DataFrame:
+    df_copy = df.copy()
+    for col in columns:
+        for i in np.arange(1, n_lag + 1):
+            df_copy[f'lag_{i}_{col}'] = df_copy.groupby(groupby)[col].shift(i)
+    return df_copy
